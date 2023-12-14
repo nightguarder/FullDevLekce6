d34b71d2b853b86e8610efda55e79a8e0b0bb987
cyrils <nigthguarder@gmail.com>
2023-12-14 18:37:29 +0100

    setup project

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,7 @@ import './App.css';
 
 function App() {
   //variables
-  const [ws, setWs] = useState(null);
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const [payload, setPayload] = useState([]);
 
-  useEffect(() => {
-    const socket = new WebSocket('ws://localhost:3000');
-    socket.onmessage = (event) => {
-      setPayload((oldPayload) => [...oldPayload, JSON.parse(event.data)]);
-    };
-    setWs(socket)
-    return () => {
-      if (socket.readyState == 1) {
-      socket.close()
-      }
-    }
-  }, []);
-
-  const sendMessage = () => {
-    if (ws) {
-      ws.send(JSON.stringify({ username, text: message }));
-      setMessage('');
-    }
-  };
 
   return (
     <>
@@ -40,12 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WebSocker + React</h1>
+      <h1>Image upload with Form Data</h1>
       <div className="card">
-        <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-        <input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" />
         <p>
-          <button onClick={sendMessage}>Send</button>
+          <button onClick={sendMessage}>Upload</button>
         </p>
       </div>
       {payload.map((message, index) => (
