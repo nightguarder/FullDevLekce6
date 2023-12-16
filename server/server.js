61cd35462f2e42419ca14941c29add79d9e04a37
cyrils <nigthguarder@gmail.com>
2023-12-17 00:26:45 +0100

    many errors...

---
+++ server/server.js
@@ -0,0 +1,57 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import fileUpload from 'express-fileupload'
+
+//dotenv config
+const HOST = process.env.HOST || 'localhost'
+const PORT = process.env.PORT || "3000"
+dotenv.config();
+
+//Express config
+const app = express()
+app.use(fileUpload())
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cors()); 
+//[Error] Fetch API cannot load https://localhost:3000/upload due to access control checks
+
+//Default endpoint
+app.get('/',(req,res) => {
+    res.send(' Nothing to see here: GET/')
+})
+
+// Project public upload path
+const uploadPath = 'public/images'
+
+//Handle upload
+//NENI MOJE TVORBA
+//https://rostislavjadavan.com/posts/designing-file-upload-endpoint-in-rest-api
+app.post("/upload", async (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  //The name has to match formData.append('myfile', upload);
+  let uploadedFile = req.files.myfile
+    //*Security risk* User can upload harmful data?  
+    await uploadedFile.mv(uploadPath + '/' + uploadedFile.name)
+    try {
+      await uploadedFile.mv(uploadPath);
+      console.log("Upload complete.")
+      res.json({
+        url: generateUrl(uploadedFile),
+        filename: uploadedFile.name,
+      });
+    } 
+    catch (err) {
+      res.status(500).send(err.message);
+    }
+  });
+  function generateUrl(uploadedFile) {
+    // Generate the URL based on the uploaded image name
+    return `http://localhost:${PORT}/uploads/images/${uploadedFile.name}`;
+  }
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running at http://${HOST}:${PORT}`);
+  });
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,49 @@
-import { useState, useEffect} from 'react';
+import { useState} from 'react';
 import reactLogo from './assets/react.svg';
 import expressLogo from '/express-js.svg';
 import './App.css';
 
 function App() {
-  //variables
+  //variable States
 
-  const [upload,setUpload] = useState(false)
+  const [file,setFile] = useState(false)
+  const [imageUrl, setImageUrl] = useState(null);
 
-  const fileChange = (e) => {
+  const handleFileChange = (e) => {
     if(e.target.files){
     setFile(e.target.files[0]);
     }
   };
 
-  const handleUpload = ()=>{
-    if (!file)
-      return;
-    
-    else{
-      try {
-
+  const handleUpload = async ()=>{
+    try {
+      
         const formData = new FormData();
-        formData.append('image', file);
-
-        //manual Headers for single file upload
+        //append file data 
+        formData.forEach((file, i) => {
+          data.append(`file-${i}`, file, file.name);
+        });
+        //fetch with temporary headers
+        //fetch the file with the upload endpoint
         const response = await fetch('http://localhost:3000/upload', {
           method: 'POST',
           body: formData,
-        });
-    }
-    catch{
-
-    }
-  }
+          headers: {
+            'Content-type': 'multipart/form-data',
+          },
+        })
+        //If the upload is complete
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setImageUrl(data.url);  
+        } else {
+          console.error('Error uploading data to the server.', response.statusText);
+        }
+      } catch (error) {
+        console.error('Something went wrong.', error.message);
+      }
+  };
 
   return (
     <>
@@ -46,10 +56,25 @@ function App() {
         </a>
       </div>
       <h1>Image upload with Form Data</h1>
+      <div className='fileDiv'>
+      <p>
+        <input type="file" className="fileInput"  onChange={handleFileChange} />
+        { file && `${file.name} - ${file.type}`}
+        </p>
+      </div>
       <div className="card">
         <p>
           <button onClick={handleUpload}>Upload</button>
         </p>
+      </div>
+      <div className='result'>
+        <p>
+        {imageUrl && (
+        <a href={imageUrl} target="_blank" rel="noopener noreferrer">
+          {imageUrl}
+        </a>
+      )}
+      </p>
       </div>
 
     </>
