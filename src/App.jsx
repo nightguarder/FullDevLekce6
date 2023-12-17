16092749fa62586545c563c3053f4ece7a4c04c7
cyrils <nigthguarder@gmail.com>
2023-12-17 01:36:04 +0100

    missing axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState} from 'react';
 import reactLogo from './assets/react.svg';
 import expressLogo from '/express-js.svg';
+import axios from 'axios'
 import './App.css';
 
 function App() {
