a2f4ebd8e4e60eb5d4e0261c0d698ff1281a7b73
cyrils <nigthguarder@gmail.com>
2023-12-17 01:35:43 +0100

    backend done

--- server/server.js
+++ server/server.js
@@ -19,16 +19,33 @@ app.get('/',(req,res) => {
     res.send(' Nothing to see here: GET/')
 })
 
+//nastaveni express static path
+app.use('/uploads', express.static(path.join(__dirname, '../app/public')));
+
+// Endpoint pro nahrávání souborů
 app.post('/upload', (req, res) => {
   // Získání souboru z požadavku
-  //the .file has to match formData.append('file', upload);
-  const file = req.files.file;
+  const file = req.files.myfile;
 
-  // Nastavení cesty pro uložení souboru
+  // Kontrola if it starts with image
+  if (!file.mimetype.startsWith('image/')) {
+    return res.status(400).json({ error: 'Invalid file type. Please upload an image' });
+  }
+
+  // Express cesta do static souboru
   const uploadPath = path.join(__dirname, '../app/public', file.name);
   // Uložení souboru
-  
+  file.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
 
+    // Vrácení URL k nahrávanému souboru
+    res.json({
+      url: `/uploads/${file.name}`,
+      filename: file.name,
+    });
+  });
 });
 
 app.listen(PORT, HOST, () => {
